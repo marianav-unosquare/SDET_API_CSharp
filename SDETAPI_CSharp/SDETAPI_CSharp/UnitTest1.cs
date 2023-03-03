b2648d90691877f4c70591f0ea5efb82a34267c4
Mariana Vives <mariana.vives@unosquare.com>
2023-03-03 11:39:10 -0600

    Fist commit

---
+++ SDETAPI_CSharp/SDETAPI_CSharp/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SDETAPI_CSharp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
